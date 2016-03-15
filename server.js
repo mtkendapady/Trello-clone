600eaf7d50368923db57b6b410ef091fcc9638f4
Dustin Myers <dustinmyers@users.noreply.github.com>
2016-03-15 16:57:23 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
-var session = require('express-session');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var morgan = require('morgan');
-var mongoose = require('mongoose');
+// STEP 1 - Import the mongojs library here
+
+
 var app = express();
+// STEP 2 - Define the "db" variable here, and define the MongoDB instance you want to use
+
 var port = 8000;
 
 
@@ -14,57 +17,89 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
-app.use(session({
-    secret: 'carpediem',
-    saveUninitialized: false,
-    resave: false
-}));
-
-var isAuthenticated = function (req, res, next) {
-    if (req.session.user) {
-        next();
-    } else {
-        return res.status(403).send('Please login first')
-    }
-}
+// STEP 3 - Build your server's endpoints with all the database commands here
 
 
-/**************** API Controller *************/
-var UserCtrl = require('./api/controllers/UserCtrl.js');
-var ListCtrl = require('./api/controllers/ListCtrl.js')
 
+//STEP 4 - In your terminal, run 'mongod', then in a new tab run 'nodemon server.js'. This will start up your
+// database instance, and then your Node server. Then, in postman, you can test all of the endpoints you just built.
+// While you test the endpoints, you can you RoboMongo to make sure they are working.
 
-/**************** API *************/
-
-app.post('/auth/login', UserCtrl.login);
-
-app.get('/auth/logout', UserCtrl.logout);
-
-app.get('/api/getLists', isAuthenticated, ListCtrl.getLists)
-
-app.post('/api/addList', isAuthenticated, ListCtrl.addList);
-
-app.post('/api/deleteList', isAuthenticated, ListCtrl.deleteList);
-
-app.post('/api/addCard', isAuthenticated, ListCtrl.addCard);
-
-app.post('/api/deleteCard', isAuthenticated, ListCtrl.deleteCard);
-
-app.post('/api/moveCard', isAuthenticated, ListCtrl.moveCard);
-
-/************ END API *************/
-
-
-/*************** DB ***************/
-var mongoUri = 'mongodb://localhost:27017/betterTodo';
-mongoose.connect(mongoUri);
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', function() {
-    console.log('connected to db at ' + mongoUri)
-})
 
 app.listen(port, function() {
     console.log('Server listening on port ' + port);
-})
+});
+
+
+//STEP 5 - What you built today won't be used in the final project. That is because in the next two classes, we 
+// will show you better ways to handle what we have done. Now... Before you say "Well, we should learn that way
+// first, shouldn't we?", just know that there is wisdom to taking this one step at a time. What we learn in the
+// next couple classes will build from what you have learned today. With all of that said, you can use the code
+// from below to build the full app again, and run it, if you would like. You should, at this point, be able to
+// plug in the code below into the right places above. You will want to comment out, or delete all of your code
+// first though. That way it won't mess up any of the code for the full app.
+
+
+///////////////////
+//// SECTION 1 ////
+///////////////////
+
+// var session = require('express-session');
+// var mongoose = require('mongoose');
+
+
+
+///////////////////
+//// SECTION 2 ////
+///////////////////
+
+// app.use(session({
+//     secret: 'carpediem',
+//     saveUninitialized: false,
+//     resave: false
+// }));
+
+// var isAuthenticated = function (req, res, next) {
+//     if (req.session.user) {
+//         next();
+//     } else {
+//         return res.status(403).send('Please login first')
+//     }
+// }
+
+
+// /**************** API Controller *************/
+// var UserCtrl = require('./api/controllers/UserCtrl.js');
+// var ListCtrl = require('./api/controllers/ListCtrl.js')
+
+
+// /**************** API *************/
+
+// app.post('/auth/login', UserCtrl.login);
+
+// app.get('/auth/logout', UserCtrl.logout);
+
+// app.get('/api/getLists', isAuthenticated, ListCtrl.getLists)
+
+// app.post('/api/addList', isAuthenticated, ListCtrl.addList);
+
+// app.post('/api/deleteList', isAuthenticated, ListCtrl.deleteList);
+
+// app.post('/api/addCard', isAuthenticated, ListCtrl.addCard);
+
+// app.post('/api/deleteCard', isAuthenticated, ListCtrl.deleteCard);
+
+// app.post('/api/moveCard', isAuthenticated, ListCtrl.moveCard);
+
+// /************ END API *************/
+
+
+// /*************** DB ***************/
+// var mongoUri = 'mongodb://localhost:27017/betterTodo';
+// mongoose.connect(mongoUri);
+
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error: '));
+// db.once('open', function() {
+//     console.log('connected to db at ' + mongoUri)
+// })
