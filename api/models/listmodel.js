0802a2f59e98649cf8e45276fe0b87004ea31eba
Dustin Myers <dustinmyers@users.noreply.github.com>
2016-03-19 01:42:49 -0600

    Update listmodel.js

--- api/models/listmodel.js
+++ api/models/listmodel.js
@@ -1,17 +1,1 @@
-var Mongoose = require('mongoose');
-var Schema = Mongoose.Schema;
-
-var List = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    cards: [{
-        title: {
-            type: String,
-            required: true
-        }
-    }]
-})
-
-module.exports = Mongoose.model('List', List);
+// Build out your model here:
