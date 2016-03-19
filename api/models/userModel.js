155400f113b48b6dc032ff5e7047dd48076daeca
Dustin Myers <dustinmyers@users.noreply.github.com>
2016-03-19 01:43:17 -0600

    Update userModel.js

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,15 +1,1 @@
-var Mongoose = require('mongoose');
-var Schema = Mongoose.Schema;
-
-var User = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    lists: [{
-        type: Schema.ObjectId,
-        ref: 'List'
-    }]
-})
-
-module.exports = Mongoose.model('User', User);
+// Build out your model here:
