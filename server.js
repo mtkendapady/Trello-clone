88f9d6ba0b0aff1961fc997bc95979b55def7550
Matangi Kendapady <mtkendapady@gmail.com>
2016-10-18 20:27:33 -0500

    day one tasks completed

--- server.js
+++ server.js
@@ -1,16 +1,44 @@
 //STEP 1 - Add Express to your Node server (make sure you have installed it...):
+var express = require( "express" );
+
 
 //STEP 5 - At the bottom of this file, you will see some commented out code. Move section 1 here:
-
+// var session = require('express-session');
+// var bodyParser = require('body-parser');
+// var cors = require('cors');
+// var morgan = require('morgan');
+// var mongoose = require('mongoose');
 
 
 //STEP 2 - var app and var port will go here:
-
+var app = express();
+var port = 8080;
 
 //STEP 6 - Finally grab section 2 at the bottom of this file and drop it here:
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(cors());
+// app.use(morgan('dev'));
+// app.use(express.static(__dirname + '/public'));
+//
+// app.use(session({
+//     secret: 'carpediem',
+//     saveUninitialized: false,
+//     resave: false
+// }));
+//
+// var isAuthenticated = function (req, res, next) {
+//     if (req.session.user) {
+//         next();
+//     } else {
+//         return res.status(403).send('Please login first')
+//     }
+// }
 
 //STEP 3 - Code for your app.listen() here:
-
+app.listen(port, function() {
+    console.log("Server started on port " + port);
+});
 
 //STEP 4 - Test your server. You should be able to run nodemon in your terminal and see your console.log that you put in your app.listen callback function. If you see that, you are in business!
 //STEP 7 - Once your own code is up, and all the code from below, you can run mongod, and then start up your server, then go to localhost:8000 (or whatever your port number is), and see the whole project run.
